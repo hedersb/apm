63a574e444ecc55cab50b4080616ac85dffb4c6f
Heder S. Bernardino <hedersb@gmail.com>
2012-01-11 17:55:38 -0200

    Implementation in C programming language updated.

--- apm.c
+++ apm.c
@@ -52,16 +52,21 @@ void calculatePenalizationCoefficients (
 	int j;
 	int l;
 	double sumObjectiveFunction = 0;
+	//foreach candidate solution
 	for( i=0; i < populationSize; i++ ) {
 
 		sumObjectiveFunction += objectiveFunctionValues[ i ];
 
 	}
+	//the absolute of the sumObjectiveFunction
 	if ( sumObjectiveFunction < 0 ) {
 		sumObjectiveFunction = -sumObjectiveFunction;
 	}
 
+	//the denominator of the equation of the penalization coefficients
 	double denominator = 0;
+	//the sum of the constraint violation values
+	//these values are recorded to be used in the next situation
 	double* sumViolation = (double*) malloc( numberOfConstraints * sizeof( double ) );
 	for( l=0; l < numberOfConstraints; l++ ) {
 
@@ -75,12 +80,14 @@ void calculatePenalizationCoefficients (
 		denominator += sumViolation[ l ] * sumViolation[ l ];
 	}
 
+	//the penalization coefficients are calculated
 	for( j=0; j < numberOfConstraints; j++ ) {
 
 		penalizationCoefficients[ j ] = ( sumObjectiveFunction / denominator ) * sumViolation[ j ];
 
 	}
 
+	//remove auxiliary variables
 	free( sumViolation );
 
 }
@@ -94,6 +101,8 @@ void calculatePenalizationCoefficients (
  * must be used after the penalization coefficients
  * have been calculated by the
  * 'calculatePenalizationCoefficients' method.
+ * Also, the penalization method assumes that the 
+ * user is trying to solve a minimization problem.
  * Parameters:
  * - fitnessValues: pointer to the fitness values 
  * which will by calculated by this method;
@@ -122,23 +131,30 @@ void calculateFitness(
 	int numberOfConstraints,
 	double* penalizationCoefficients ) {
 
-	
+	//indicates if the candidate solution is infeasible
 	_Bool infeasible;
 	int i;
 	int j;
+	//the penalization value
 	double penalization;
 	for( i=0; i < populationSize; i++ ) {
 	
+		//the candidate solutions are assumed feasibles
 		infeasible = 0;
 		penalization = 0;
 		
 		for( j=0; j < numberOfConstraints; j++ ) {
 			
+			//the candidate solution is infeasible if some constraint is violated
 			infeasible |= constraintViolationValues[ i ][ j ] > 0 ;
+			//the penalization value is updated
 			penalization += penalizationCoefficients[ j ] * constraintViolationValues[ i ][ j ];
 
 		}
 
+		//the fitness is the sum of the objective function and penalization values 
+		//if the candidate solution is infeasible and just the objective function value,
+		//otherwise
 		fitnessValues[ i ] = infeasible ? objectiveFunctionValues[ i ] + penalization : objectiveFunctionValues[ i ];
 
 	}
